60c9d4f1ce7f676753ca2618444383c18462d701
Gabriel Vitor Sales Machado <gabriel.machado@loft.com.br>
2023-01-06 19:46:53 -0300

    feature: implement autocmd to execute PackerSync on write in pluginList.lus

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost pluginList.lua source <afile> | PackerSync
+  augroup end
+]])
+
 require "core.options"
 require "pluginList"
 require "core.mappings".utils()
